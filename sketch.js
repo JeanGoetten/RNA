04ecd6565cfc06aaa6e369d841aa9335941e617d
Selbstdenker <34884262+Selbstdenker@users.noreply.github.com>
2019-05-15 18:19:36 -0300

    wip

--- sketch.js
+++ sketch.js
@@ -1,6 +1,35 @@
-function setup(){
+var train = true;
 
-    var nn = new RNA(1, 3, 2)
-    let arr = [1, 2]
-    nn.train(arr, [0, 1])
+function setup() {
+    createCanvas(500, 500);
+    background(0);
+
+    nn = new RNA(2, 10, 1);
+
+    // XOR Problem
+    dataset = {
+        inputs:
+            [[1, 1],
+            [1, 0],
+            [0, 1],
+            [0, 0]],
+        outputs:
+            [[0],
+            [1],
+            [1],
+            [0]]
+    }
+}
+
+function draw() {
+    if (train) {
+        for (var i = 0; i < 10000; i++) {
+            var index = floor(random(4));
+            nn.train(dataset.inputs[index], dataset.outputs[index]);
+        }
+        if (nn.predict([0, 0])[0] < 0.000001 && nn.predict([1, 0])[0] > 0.99) {
+            train = false;
+            console.log("terminou");
+        }
+    }
 }
